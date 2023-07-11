d30fa49b2dc1fe53aea35d7593f3c30b901a111a
Magowntown <amagown@gmail.com>
2023-07-11 14:20:51 -0400

    Boiler Removed

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,14 @@ import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
         <Logos />
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+
         <MyButton />
     </div>
+
   )
 }
 
